f631c07f3a2bbd6e4468b14f5ff75186dec452b5
xfbravo <2102356261@qq.com>
2026-05-28 23:16:29 +0800

    对齐 Android Studio 支持的 AGP 版本

--- im_android/build.gradle.kts
+++ im_android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "9.2.0" apply false
+    id("com.android.application") version "9.1.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
-    id("com.google.devtools.ksp") version "2.3.7" apply false
+    id("com.google.devtools.ksp") version "2.3.5" apply false
 }
